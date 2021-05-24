00ac54116f8fbdc44ef0575effa8459f4520ed4f
Elo <eloissemolina@gmail.com>
2021-05-23 18:53:27 -0600

    fix(searchbar): fixed searchbar :bug:

--- src/components/card/Loading.jsx
+++ src/components/card/Loading.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { Card, Avatar } from "antd";
-import CardComponent from "./Card";
-// import store from "../../store/main/store";
 
 const { Meta } = Card;
 
 const LoadingCard = () => {
-  /* const { getState } = store;
-  const { STORE_REDUCER } = getState();
-  const { products } = STORE_REDUCER;
-  console.log(products); */
   return (
     <>
       <Card style={{ width: 270, marginTop: 16 }} loading>
@@ -21,8 +15,6 @@ const LoadingCard = () => {
           description="This is the description"
         />
       </Card>
-      <h1>todo</h1>
-      <CardComponent />
     </>
   );
 };
