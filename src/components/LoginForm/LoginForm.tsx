f0083df5533254fe94818d3f91fa3c8b7605fb00
Antony Giomar <49871651+antonygiomarx@users.noreply.github.com>
2021-10-05 11:57:27 -0600

    feat: refactor login page :sparkles:

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,9 +1,10 @@
-import { useRef } from "react";
-import { Button, Checkbox, Input, Form } from "antd";
+import { CSSProperties, useRef, useState } from "react";
+import { Button, Checkbox, Input, Form, Typography, Layout, Card } from "antd";
 import { nanoid as uuid } from "nanoid";
 import { Auth, signInWithEmailAndPassword } from "firebase/auth";
 import { useAuth } from "reactfire";
 import { useRouter } from "next/router";
+import Logo from "@components/Logo/Logo";
 
 export interface EmailAndPasswordAuth {
   auth: Auth;
@@ -11,10 +12,52 @@ export interface EmailAndPasswordAuth {
   password: string;
 }
 
+const formStyles = {
+  display: "grid",
+  placeItems: "center",
+} as CSSProperties;
+
+const notRegisteredStyles = {
+  flexDirection: "column",
+  display: "grid",
+  placeItems: "center",
+} as CSSProperties;
+
+const mainLayoutStyles = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+} as CSSProperties;
+
+const mainLayoutContent = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  maxWidth: "600px",
+} as CSSProperties;
+
+const mainCardStyles = {
+  borderRadius: "20px",
+  width: "500px",
+  height: "600px",
+  display: "grid",
+  placeItems: "center",
+} as CSSProperties;
+
+const logoCardStyles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "baseline",
+  marginBottom: "10px",
+} as CSSProperties;
+
+const notRegisteredTextStyles = { marginBottom: "10px" } as CSSProperties;
+
 const signIn = async ({ auth, email, password }: EmailAndPasswordAuth) => {
   try {
     await signInWithEmailAndPassword(auth, email, password);
-
     return true;
   } catch (error) {
     console.log(error);
@@ -25,90 +68,121 @@ const signIn = async ({ auth, email, password }: EmailAndPasswordAuth) => {
 export const LoginForm = (): JSX.Element => {
   const passRef = useRef<Input>(null);
   const emailRef = useRef<Input>(null);
+
+  const [loginLoading, setLoginLoading] = useState(false);
+  const [registerLoading, setRegisterLoading] = useState(false);
+
   const { push } = useRouter();
+  const { Item } = Form;
+  const { Text, Title } = Typography;
+  const { Content } = Layout;
 
   const auth = useAuth();
 
+  const handleLogin = async () => {
+    setLoginLoading(true);
+    const logged = await signIn({
+      auth,
+      email: emailRef.current?.state?.value,
+      password: passRef.current?.state?.value,
+    });
+    setLoginLoading(false);
+    if (logged) {
+      push("/");
+    }
+  };
+
+  const handleNotRegisteredClick = () => {
+    setRegisterLoading(true);
+    push("/register");
+    setRegisterLoading(false);
+  };
+
   return (
-    <Form key={uuid()} className="form" name="login" autoComplete="on">
-      <Form.Item
-        key={uuid()}
-        label="Correo"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: "Por favor ingrese su correo",
-          },
-        ]}
-      >
-        <Input
-          key={uuid()}
-          ref={emailRef}
-          placeholder="Correo"
-          name="email"
-          type="email"
-        />
-      </Form.Item>
-      <Form.Item
-        key={uuid()}
-        label="Contraseña"
-        name="contraseña"
-        rules={[
-          {
-            required: true,
-            message: "Por favor ingrese su contraseña",
-          },
-        ]}
-      >
-        <Input.Password
-          key={uuid()}
-          placeholder="Contraseña"
-          name="contraseña"
-          type="password"
-          ref={passRef}
-          onPressEnter={() => {
-            (async () => {
-              const logged = await signIn({
-                auth,
-                email: emailRef.current?.state?.value,
-                password: passRef.current?.state?.value,
-              });
+    <Layout style={mainLayoutStyles}>
+      <Card bordered style={mainCardStyles}>
+        <Content style={logoCardStyles}>
+          <Logo />
+          <Title level={3}>Mi Ventecita</Title>
+        </Content>
+        <Content style={mainLayoutContent}>
+          <Form style={formStyles} key={uuid()} name="login" autoComplete="on">
+            <Item
+              key={uuid()}
+              label="Correo"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Por favor ingrese su correo",
+                },
+              ]}
+            >
+              <Input
+                key={uuid()}
+                ref={emailRef}
+                placeholder="Correo"
+                name="email"
+                type="email"
+              />
+            </Item>
+            <Item
+              key={uuid()}
+              label="Contraseña"
+              name="contraseña"
+              rules={[
+                {
+                  required: true,
+                  message: "Por favor ingrese su contraseña",
+                },
+              ]}
+            >
+              <Input.Password
+                key={uuid()}
+                placeholder="Contraseña"
+                name="contraseña"
+                type="password"
+                ref={passRef}
+                onPressEnter={handleLogin}
+              />
+            </Item>
 
-              if (logged) {
-                push("/");
-              }
-            })();
-          }}
-        />
-      </Form.Item>
-
-      <Checkbox key={uuid()}>Recuerdame</Checkbox>
-
-      <Button key={uuid()} type="link" className="linkButton">
-        ¿Olvidaste tu contraseña?
-      </Button>
-
-      <Button
-        key={uuid()}
-        type="primary"
-        className="button"
-        onClick={() => {
-          (async () => {
-            const logged = await signIn({
-              auth,
-              email: emailRef.current?.state?.value,
-              password: passRef.current?.state?.value,
-            });
-
-            if (logged) {
-              push("/");
-            }
-          })();
-        }}
-      >
-        Iniciar sesión
-      </Button>
-    </Form>
+            <Item>
+              <Checkbox key={uuid()}>Recuerdame</Checkbox>
+            </Item>
+            <Item>
+              <Button key={uuid()} type="link" className="linkButton">
+                ¿Olvidaste tu contraseña?
+              </Button>
+            </Item>
+            <Item>
+              <Button
+                loading={loginLoading}
+                key={uuid()}
+                type="primary"
+                onClick={handleLogin}
+              >
+                Iniciar sesión
+              </Button>
+            </Item>
+            <Item>
+              <Content style={notRegisteredStyles}>
+                <Text style={notRegisteredTextStyles}>
+                  ¿Aún no te has registrado?
+                </Text>
+                <Button
+                  onClick={handleNotRegisteredClick}
+                  type="ghost"
+                  key={uuid()}
+                  loading={registerLoading}
+                >
+                  Registrate
+                </Button>
+              </Content>
+            </Item>
+          </Form>
+        </Content>
+      </Card>
+    </Layout>
   );
 };
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,44 +1,19 @@
-import { Button } from "antd";
+import { Layout } from "antd";
 import Head from "next/head";
-import { useRouter } from "next/router";
 
 import { LoginForm } from "@components/LoginForm/LoginForm";
-import Logo from "@components/Logo/Logo";
-
-const RegisterButton = (): JSX.Element => {
-  const { push } = useRouter();
-  return (
-    <div>
-      <h4 style={{ fontWeight: "bold", marginTop: "-15px" }}>
-        ¿Aún no te has registrado?
-      </h4>
-      <Button
-        type="default"
-        className="button"
-        style={{ width: "50%", left: "25%", textAlign: "center" }}
-        onClick={() => {
-          push("/register");
-        }}
-      >
-        Regístrate
-      </Button>
-    </div>
-  );
-};
 
 const Login = (): JSX.Element => (
   <>
     <Head>
       <title>Inicio de sesión - Mi Ventecita</title>
     </Head>
-    <div className="login">
-      <div>
-        <Logo />
+    <Layout className="login">
+      <Layout>
         <LoginForm />
-        <RegisterButton />
-      </div>
-      <div className="rightside" />
-    </div>
+      </Layout>
+      <Layout className="rightside" />
+    </Layout>
   </>
 );
 
